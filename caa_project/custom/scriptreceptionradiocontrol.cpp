8429feb2e29dd1f5986eeef8067a26be6253eec3
user <jranstrom@usf.edu>
2024-11-26 12:35:43 -0500

    Added basic functionallity to script reception

--- caa_project/custom/scriptreceptionradiocontrol.cpp
+++ caa_project/custom/scriptreceptionradiocontrol.cpp
@@ -24,8 +24,173 @@ ScriptReceptionRadioControl::ScriptReceptionRadioControl(QWidget *parent, std::s
 
     mainSpacer = new QSpacerItem(5,5,QSizePolicy::Expanding,QSizePolicy::Expanding);
 
+    QWidget * scriptEngineContainer = new QWidget;
+    scriptEngineLayout = new QGridLayout(scriptEngineContainer);
+
+    scriptEngineField = new LabelandFieldWidget(this,"Engine name:","Engine_1",true);
+    scriptNameField = new LabelandFieldWidget(this,"Script name:","adalm_pluto_tx",true);
+
+    QWidget * engineStatusContainer = new QWidget;
+    QHBoxLayout * engineStatusLayout = new QHBoxLayout(engineStatusContainer);
+
+    QSpacerItem * engineStatusLeftSpacer = new QSpacerItem(5,5,QSizePolicy::Expanding,QSizePolicy::Fixed);
+    engineConnectedIndidicator = new IndicatorButtonWidget(this);
+    engineConnectedIndidicator->setState(1);
+    engineConnectBtn = new QPushButton("Connect",this);
+    connect(engineConnectBtn,&QPushButton::released,this,&ScriptReceptionRadioControl::onConnectEngineBtnRelease);
+    QSpacerItem * engineStatusRightSpacer = new QSpacerItem(5,5,QSizePolicy::Expanding,QSizePolicy::Fixed);
+
+    engineStatusLayout->addItem(engineStatusLeftSpacer);
+    engineStatusLayout->addWidget(engineConnectedIndidicator);
+    engineStatusLayout->addWidget(engineConnectBtn);
+    engineStatusLayout->addItem(engineStatusRightSpacer);
+
+    scriptEngineLayout->addWidget(scriptEngineField,0,0);
+    scriptEngineLayout->addWidget(engineStatusContainer,1,0);
+    scriptEngineLayout->addWidget(scriptNameField,0,1);
+
+
+    // Control Section
+    QWidget * controlSectionContainer = new QWidget;
+    //controlSectionContainer->setSizePolicy(QSizePolicy::Fixed,QSizePolicy::Fixed);
+    controlSectionLayout = new QVBoxLayout(controlSectionContainer);
+
+    QWidget * statusSectionContainer = new QWidget;
+    //statusSectionContainer->setSizePolicy(QSizePolicy::Fixed,QSizePolicy::Fixed);
+    statusSectionLayout = new QHBoxLayout(statusSectionContainer);
+
+    QSpacerItem * statusLeftSpacer = new QSpacerItem(5,5,QSizePolicy::Expanding,QSizePolicy::Fixed);
+    receptionStatusIndicator = new IndicatorButtonWidget(this);
+    receptionStatusLabel = new QLabel("No ongoing reception",this);
+    toggleReceptionBtn = new QPushButton("Start",this);
+    connect(toggleReceptionBtn,&QPushButton::released,this,&ScriptReceptionRadioControl::onToggleReceptionBtnRelease);
+    QSpacerItem * statusRightSpacer = new QSpacerItem(5,5,QSizePolicy::Expanding,QSizePolicy::Fixed);
+
+    statusSectionLayout->addItem(statusLeftSpacer);
+    statusSectionLayout->addWidget(receptionStatusIndicator);
+    statusSectionLayout->addWidget(receptionStatusLabel);
+    statusSectionLayout->addWidget(toggleReceptionBtn);
+    statusSectionLayout->addItem(statusRightSpacer);
+    //connect(toggleReceptionBtn,&QPushButton::released,this,&continousReceptionRadioControl::onToggleReceptionBtnRelease);
+
+    QFrame * IandCDivider = new QFrame();IandCDivider->setFrameShape(QFrame::HLine);IandCDivider->setFrameShadow(QFrame::Sunken);
+    QFrame * divider2 = new QFrame();divider2->setFrameShape(QFrame::HLine);divider2->setFrameShadow(QFrame::Sunken);
+
 
     mainLayout->addWidget(infoSectionContainerWidget);
+    mainLayout->addWidget(IandCDivider);
+    mainLayout->addWidget(scriptEngineContainer);
+    mainLayout->addWidget(divider2);
+    mainLayout->addWidget(statusSectionContainer);
     mainLayout->addItem(mainSpacer);
 
+    receptionProcessTimer.setInterval(1000);
+    receptionProcessTimer.setSingleShot(false);
+    receptionProcessTimer.start();
+    connect(&receptionProcessTimer,&QTimer::timeout,this,&ScriptReceptionRadioControl::onReceptionProcessTimerTick);
+
+}
+
+void ScriptReceptionRadioControl::onControlClose()
+{
+    receptionProcessTimer.stop();
+    disconnect(&receptionProcessTimer,&QTimer::timeout,this,&ScriptReceptionRadioControl::onReceptionProcessTimerTick);
+
+    if(isReceiving == false){
+        return;
+    }
+
+    cRadioResponse response = sourceRadio->stopContinousReception();
+
+    if(response.code == 0){
+        isReceiving = false;
+        emit statusUpdateRequest("Reception terminated",-1);
+    }else{
+        emit statusUpdateRequest(response.message,-1);
+    }
+}
+
+void ScriptReceptionRadioControl::onToggleReceptionBtnRelease()
+{
+    cRadioResponse response;
+    std::string statusUpdate_m = "";
+    int statusUpdateCode = 0;
+
+    if(isReceiving == false){
+        response = sourceRadio->startContinousReception();
+
+        if(response.code == 0){
+            statusUpdate_m = "Reception started";
+            statusUpdateCode = 0;
+        }else{
+            statusUpdate_m = response.message;
+            statusUpdateCode = -1;
+        }
+
+    }else{
+        response = sourceRadio->stopContinousReception();
+        if(response.code == 0){
+            statusUpdate_m = "Reception stopped";
+            statusUpdateCode = 0;
+        }else{
+            statusUpdate_m = response.message;
+            statusUpdateCode = -1;
+        }
+    }
+
+    if(statusUpdate_m != ""){
+        emit statusUpdateRequest(statusUpdate_m,statusUpdateCode);
+    }
+}
+
+void ScriptReceptionRadioControl::onConnectEngineBtnRelease()
+{
+    if(engineConnected == false){
+        std::vector<std::u16string> names = matlab::engine::findMATLAB();
+        std::vector<std::u16string>::iterator it;
+        std::string engineName_t = scriptEngineField->getFieldText();
+
+        it = std::find(names.begin(),names.end(),stringToU16String(engineName_t));
+        if(it != names.end()){
+            matlabPtr = matlab::engine::connectMATLAB(*it);
+        }
+
+        if(matlabPtr){
+            matlab::data::ArrayFactory factory;
+            matlab::data::CharArray arg = factory.createCharArray("-release");
+            matlab::data::CharArray version = matlabPtr->feval(u"version",arg);
+            engineConnectedIndidicator->setState(2);
+            emit statusUpdateRequest("Connected to " + version.toAscii(),1);
+            engineConnected = true;
+        }else{
+            emit statusUpdateRequest("Could not connect to matlab engine " + engineName_t,-1);
+        }
+    }
+}
+
+void ScriptReceptionRadioControl::onReceptionProcessTimerTick()
+{
+    bool cStatus = sourceRadio->continous_reception_running;
+    if(cStatus != isReceiving){
+        isReceiving = cStatus;
+        if(isReceiving == true){
+            toggleReceptionBtn->setText("Stop");
+            receptionStatusIndicator->setState(2);
+            receptionStatusLabel->setText("Reception in progress");
+        }else{
+            toggleReceptionBtn->setText("Start");
+            receptionStatusIndicator->setState(1);
+            receptionStatusLabel->setText("No ongoing reception");
+        }
+        return;
+    }
+}
+
+std::u16string ScriptReceptionRadioControl::stringToU16String(const std::string &str)
+{
+    std::wstring_convert<std::codecvt_utf8<char32_t>,char32_t> convert32;
+
+    auto u32string = convert32.from_bytes(str);
+    std::u16string u16str(u32string.begin(),u32string.end());
+    return u16str;
 }
--- caa_project/custom/scriptreceptionradiocontrol.h
+++ caa_project/custom/scriptreceptionradiocontrol.h
@@ -3,8 +3,13 @@
 
 #include <QWidget>
 #include <qboxlayout.h>
+#include <qpushbutton.h>
+#include <qtimer.h>
+#include "indicatorbuttonwidget.h"
 #include "labelandfieldwidget.h"
+#include "labelandspinboxwidget.h"
 #include "radiocontrolbasewidget.h"
+#include "MatlabEngine.hpp"
 
 class ScriptReceptionRadioControl : public RadioControlBaseWidget
 {
@@ -12,18 +17,53 @@ class ScriptReceptionRadioControl : public RadioControlBaseWidget
 public:
     explicit ScriptReceptionRadioControl(QWidget *parent = nullptr, std::shared_ptr<cRadioObject> rad_p=nullptr);
 
-    void onControlClose() override {};
+    void onControlClose() override;
 
     std::string getType() override {return "script-rx";}
 
+private slots:
+    void onToggleReceptionBtnRelease();
+    void onConnectEngineBtnRelease();
+    void onReceptionProcessTimerTick();
 private:
+
+    bool isReceiving = false;
+
+    std::unique_ptr<matlab::engine::MATLABEngine> matlabPtr;
+    bool engineConnected = false;
+    std::u16string stringToU16String(const std::string &str);
 
     QVBoxLayout * mainLayout;
 
-    QGridLayout * infoSectionLayout;
+     QGridLayout * infoSectionLayout;
     LabelandFieldWidget * serialInfoField;
     LabelandFieldWidget * typeInfoField;
     LabelandFieldWidget * addressInfoField;
+
+    LabelandFieldWidget * capturePathField;
+    QPushButton * browsePathBtn;
+    QLineEdit * captureModifierField;
+    QLineEdit * captureExtentionField;
+    QLabel * captureDotLabel;
+
+    QHBoxLayout * statusSectionLayout;
+    QHBoxLayout * controlandCaptureLayout;
+    QLabel * receptionStatusLabel;
+    IndicatorButtonWidget * receptionStatusIndicator;
+
+    QGridLayout * scriptEngineLayout;
+    LabelandFieldWidget * scriptEngineField;
+    LabelandFieldWidget * scriptNameField;
+    IndicatorButtonWidget * engineConnectedIndidicator;
+    QPushButton * engineConnectBtn;
+
+    QVBoxLayout * controlSectionLayout;
+    QPushButton * toggleReceptionBtn;
+    QPushButton * saveCaptureBtn;
+    LabelandSpinBoxWidget * fftLengthSpinBox;
+    LabelandSpinBoxWidget * processTimerSpinBox;
+
+    QTimer receptionProcessTimer;
 
     QSpacerItem * mainSpacer;
 };
